f94d767ea98e81f4eee194bbac5333ae5c6133c7
enzo <trompeneers@telenet.be>
2016-10-06 16:22:10 +0200

    opdracht-comments toegevoegd"

---
+++ opdracht-comments/index.php
@@ -0,0 +1,23 @@
+<?php
+    /*
+    naam: enzo trompeneers
+    e-mail:enzo.trompeneers@student.kdg.be
+    */
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht comments</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        <section class="body">
+			<h1>Opdracht comments: deel 1</h1>
+			<p><?php echo "enzo trompeneers" ?></p>
+		</section>
+    </body>
+</html>
