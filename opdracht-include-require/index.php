3a4d2678a6b2ea78084e67ed067b44bd3993c90b
enzo <trompeneers@telenet.be>
2016-11-15 20:15:08 +0100

    opdracht-include-require toegevoegd

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht include/require</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+        li {
+            float: left;
+        }
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            text-decoration: none;
+            margin: 20px;
+        }
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+    </style>
+    <body>
+        <header>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#news">News</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+        </header>
+        <h1>Web-backend opdracht</h1>
+        <h2>Opdracht include/require</h2>
+        <footer>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#news">News</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+        </footer>
+    </body>
+</html>
