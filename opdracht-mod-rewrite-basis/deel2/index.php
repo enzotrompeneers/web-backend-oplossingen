4e6ab181bb19e4921063d3b104ba01edebbdb2a7
enzo <trompeneers@telenet.be>
2017-01-02 16:37:09 +0100

    opdracht-mod-rewrite deel 2

---
+++ opdracht-mod-rewrite-basis/deel2/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Index pagina</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        <section class="body">
+            <h1>Index</h1>
+
+            <?= (isset($_GET))? var_dump($_GET): "no get" ?>
+            
+        </section>    
+    </body>
+</html>
