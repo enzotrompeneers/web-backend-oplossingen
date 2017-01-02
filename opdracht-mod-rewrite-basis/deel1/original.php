ab7c4cc11f9a59b4411dadba22a45e8a6a90eb23
enzo <trompeneers@telenet.be>
2017-01-02 15:50:07 +0100

    opdracht-mod-rewrite deel 1 toegevoegd

---
+++ opdracht-mod-rewrite-basis/deel1/original.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Het originele bestand.</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        <section class="body">
+            <h1>Original</h1>
+
+            
+
+            
+        </section>    
+    </body>
+</html>
