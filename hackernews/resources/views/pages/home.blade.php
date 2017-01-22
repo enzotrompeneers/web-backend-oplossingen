4d121ebd88bcc0c22f8e631ee745d7bd1c56ab39
enzotrompeneers <trompeneers@telenet.be>
2017-01-23 00:02:11 +0100

    view home extends app

--- hackernews/resources/views/pages/home.blade.php
+++ hackernews/resources/views/pages/home.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to the homepage, {{$name}}</h1>
-</body>
-</html>
+@extends('app')
+
+@section('content')
+<h1>Welcome to the homepage, {{$name}}</h1>
+@stop
