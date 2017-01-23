aa763e3cfce5d90ed458f4c2016ae839d9313cd9
enzotrompeneers <trompeneers@telenet.be>
2017-01-23 18:52:34 +0100

    view home content styling

--- hackernews/resources/views/pages/home.blade.php
+++ hackernews/resources/views/pages/home.blade.php
@@ -1,5 +1,51 @@
 @extends('app')
 
 @section('content')
-<h1>Welcome to the homepage, {{$name}}</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Article overview</div>
+                    <div class="panel-content">
+                        <ul class="article-overview">
+                            <li>
+                                <div class="vote">                
+                                    <div class="form-inline upvote">
+                                        <i class="fa fa-btn fa-caret-up disabled upvote" title="You need to be logged in to upvote"></i>
+                                    </div>
+                                    <div class="form-inline upvote">
+                                        <i class="fa fa-btn fa-caret-down disabled downvote" title="You need to be logged in to downvote"></i>
+                                    </div>
+                                </div>
+                                <div class="url">
+                                    <a href="http://arstechnica.com/gaming/2016/07/scythe-the-most-hyped-board-game-of-2016-delivers/" class="urlTitle">Scythe, the most-hyped board game of 2016, delivers</a>
+                                </div> 
+                                <div class="info">
+                                    7 points  | posted by Tomte | <a href="comments/1">4 comments</a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="vote">
+                                    <div class="form-inline upvote">
+                                        <i class="fa fa-btn fa-caret-up disabled upvote" title="You need to be logged in to upvote"></i>
+                                    </div>
+                                    <div class="form-inline upvote">
+                                        <i class="fa fa-btn fa-caret-down disabled downvote" title="You need to be logged in to downvote"></i>
+                                    </div>
+                                </div>
+                                <div class="url">
+                                    <a href="https://blog.mozilla.org/security/2016/08/01/enhancing-download-protection-in-firefox/" class="urlTitle">Enhancing Download Protection in Firefox</a>
+                                </div> 
+                                <div class="info">
+                                    3 points  | posted by eto3 | <a href="comments/4">2 comments</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
