6fc4ad50217e4ab0eca28d146ef842c19aebcb55
enzo <trompeneers@telenet.be>
2017-01-24 21:25:05 +0100

    add route delete article and delete comment

--- hacker-news/routes/web.php
+++ hacker-news/routes/web.php
@@ -16,10 +16,14 @@ Route::get('/', 'Controller@home');
 Route::get('home', 'Controller@home');
 
 Route::get('article/add', 'Controller@addArticle');
-Route::get('article/edit/{article}', 'Controller@editArticle');
+Route::get('article/edit/{articleID}', 'Controller@editArticle');
+Route::get('article/delete/{articleID}', 'Controller@editArticle');
 
-Route::get('comments/{articleID}', 'Controller@addComment');
-Route::get('comments/edit/{comment}', 'Controller@editComment');
+
+Route::get('comments/{articleID}', 'Controller@showComments');
+Route::get('comments/add/{articleID}', 'Controller@addComment');
+Route::get('comments/edit/{commentID}', 'Controller@editComment');
+Route::get('comments/delete/{commentID}', 'Controller@deleteComment');
 
 Route::get('login', 'Controller@login');
 Route::get('register', 'Controller@register');
