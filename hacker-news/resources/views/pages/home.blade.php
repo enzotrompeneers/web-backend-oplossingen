97fecb18d9b5565cec023e230a36a85b465e687c
enzo <trompeneers@telenet.be>
2017-01-24 19:12:59 +0100

    view home show articles

--- hacker-news/resources/views/pages/home.blade.php
+++ hacker-news/resources/views/pages/home.blade.php
@@ -8,39 +8,24 @@
                 <div class="panel-heading">Article overview</div>
                     <div class="panel-content">
                         <ul class="article-overview">
-                            <li>
-                                <div class="vote">                
-                                    <div class="form-inline upvote">
-                                        <i class="fa fa-btn fa-caret-up disabled upvote" title="You need to be logged in to upvote"></i>
+                            @foreach ($articles as $article)
+                                <li>
+                                    <div class="vote">                
+                                        <div class="form-inline upvote">
+                                            <i class="fa fa-btn fa-caret-up disabled upvote" title="You need to be logged in to upvote"></i>
+                                        </div>
+                                        <div class="form-inline upvote">
+                                            <i class="fa fa-btn fa-caret-down disabled downvote" title="You need to be logged in to downvote"></i>
+                                        </div>
                                     </div>
-                                    <div class="form-inline upvote">
-                                        <i class="fa fa-btn fa-caret-down disabled downvote" title="You need to be logged in to downvote"></i>
+                                    <div class="url">
+                                        <a href="{{$article->url}}" class="urlTitle">{{$article->title}}</a>
+                                    </div> 
+                                    <div class="info">
+                                        7 points  | posted by Tomte | <a href="comments/1">4 comments</a>
                                     </div>
-                                </div>
-                                <div class="url">
-                                    <a href="http://arstechnica.com/gaming/2016/07/scythe-the-most-hyped-board-game-of-2016-delivers/" class="urlTitle">Scythe, the most-hyped board game of 2016, delivers</a>
-                                </div> 
-                                <div class="info">
-                                    7 points  | posted by Tomte | <a href="comments/1">4 comments</a>
-                                </div>
-                            </li>
-
-                            <li>
-                                <div class="vote">
-                                    <div class="form-inline upvote">
-                                        <i class="fa fa-btn fa-caret-up disabled upvote" title="You need to be logged in to upvote"></i>
-                                    </div>
-                                    <div class="form-inline upvote">
-                                        <i class="fa fa-btn fa-caret-down disabled downvote" title="You need to be logged in to downvote"></i>
-                                    </div>
-                                </div>
-                                <div class="url">
-                                    <a href="https://blog.mozilla.org/security/2016/08/01/enhancing-download-protection-in-firefox/" class="urlTitle">Enhancing Download Protection in Firefox</a>
-                                </div> 
-                                <div class="info">
-                                    3 points  | posted by eto3 | <a href="comments/4">2 comments</a>
-                                </div>
-                            </li>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
