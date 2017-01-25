8159582ce9ad5e29d34eff57366693e8be86e62c
enzo <trompeneers@telenet.be>
2017-01-25 23:11:55 +0100

    user to logout and changed urls

--- hacker-news/resources/views/app.blade.php
+++ hacker-news/resources/views/app.blade.php
@@ -114,17 +114,35 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="../home">Home</a></li>
+                    <li><a href="{{url('home')}}">Home</a></li>
                      @unless (Auth::guest())
-                         <li><a href="../article/add">Add article</a></li>
+                         <li><a href="{{url('article/add')}}">Add article</a></li>
                     @endunless
                 </ul>
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                    <li><a href="../login">Login</a></li>
-                    <li><a href="../register">Register</a></li>
+                    <li><a href="{{url('login')}}">Login</a></li>
+                    <li><a href="{{url('register')}}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{url('logout')}}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        <i class="fa fa-btn fa-sign-out"></i>
+                                        Logout
+                                    </a>
+                                    <form id="logout-form" action="{{url('logout')}}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
                     @endif
                 </ul>
             </div>
